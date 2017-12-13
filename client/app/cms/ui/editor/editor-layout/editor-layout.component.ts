45fbad556e2c1cbd435977fee46bd893123d7881
NITECO\hung.dang <danghung1202@gmail.com>
2017-12-13 18:32:22 +0700

    Save content with unique url

--- client/app/cms/ui/editor/editor-layout/editor-layout.component.ts
+++ client/app/cms/ui/editor/editor-layout/editor-layout.component.ts
@@ -5,32 +5,4 @@ import { PAGE_TYPE_METADATA_KEY, PROPERTY_METADATA_KEY, PROPERTIES_METADATA_KEY 
 @Component({
     templateUrl: './editor-layout.component.html',
 })
-export class EditorLayoutComponent {
-    title = 'app';
-    selectedPage: any;
-    pages: Array<any> = [];
-
-    setSelectedPage(page) {
-        this.selectedPage = page;
-    }
-
-    ngOnInit() {
-        CMS.PAGE_TYPES.forEach(pageType => {
-            let metadata = Reflect.getMetadata(PAGE_TYPE_METADATA_KEY, pageType);
-            for (var key in pageType) {
-                console.log(key);
-            }
-            let properties = Reflect.getMetadata(PROPERTIES_METADATA_KEY, pageType);
-            let propertiesMetadata = [];
-            if (properties)
-                properties.forEach(element => {
-                    propertiesMetadata.push(Reflect.getMetadata(PROPERTY_METADATA_KEY, pageType, element))
-                });
-            this.pages.push({
-                type: pageType,
-                metadata: metadata,
-                properties: propertiesMetadata
-            })
-        })
-    }
-}
+export class EditorLayoutComponent {}
--- server/controllers/content.ts
+++ server/controllers/content.ts
@@ -1,6 +1,54 @@
 import Content from '../models/content';
 import BaseCtrl from './base';
 
+const emptyObjectId = '000000000000000000000000'
+
 export default class ContentCtrl extends BaseCtrl {
   model = Content;
+
+  insert = (req, res) => {
+    const obj = new this.model(req.body);
+    let nameInUrl = obj.nameInUrl;
+    this.model.findOne({ _id: obj.parentId }).then(parent => {
+      let parentId = parent ? `${parent._id}` : emptyObjectId;
+      this.generateNameInUrl(0, nameInUrl, parentId).then(url => {
+        obj.nameInUrl = url;
+        obj.linkUrl = parent ? `${parent.linkUrl}/${url}` : `/${url}`;
+        obj.parentId = parentId;
+        obj.save((err, item) => {
+          // 11000 is the code for duplicate key error
+          if (err && err.code === 11000) {
+            res.sendStatus(400);
+          }
+          if (err) {
+            return console.error(err);
+          }
+          res.status(200).json(item);
+        });
+      })
+    })
+
+  }
+
+  private generateNameInUrl = (index: number, orignalUrl: string, parentId: string, generatedNameInUrl?: string): any =>
+    this.model.count({ 'nameInUrl': generatedNameInUrl ? generatedNameInUrl : orignalUrl, 'parentId': parentId })
+      .then(count => {
+        console.log(count);
+        if (count > 0) {
+          return this.generateNameInUrl(index + 1, orignalUrl, parentId, `${orignalUrl}-${index + 1}`);
+        }
+        return generatedNameInUrl ? generatedNameInUrl : orignalUrl;
+      })
+      .catch(err => {
+        return console.error(err);
+      })
+
+
+  getByUrl = (req, res) => {
+    this.model.findOne({ linkUrl: req.query.url }, (err, item) => {
+      if (err) { return console.error(err); }
+      res.status(200).json(item);
+    });
+  }
+
 }
