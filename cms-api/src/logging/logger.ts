24ae3466f2240f0cd47b173e7c4fb170645ea5f8
hung.dang <danghung1202@gmail.com>
2019-02-01 17:59:57 +0700

    Refactor winston logger

--- cms-api/src/logging/logger.ts
+++ cms-api/src/logging/logger.ts
@@ -1,12 +1,9 @@
 import * as winston from 'winston';
-const DailyRotateFile = require('winston-daily-rotate-file');
 
 import { CONFIG } from '../config/config';
 import { HttpException } from '../errorHandling';
-import { errorStackTracerFormat, getLogDir } from './winstonHelper';
+import { errorStackTracerFormat, getTransports } from './winstonHelper';
 import { MorganToken } from './morganHelper';
-
-const logDir = getLogDir();
 
 const options: winston.LoggerOptions = {
     level: CONFIG.LOG.LEVEL,
@@ -17,34 +14,10 @@ const options: winston.LoggerOptions = {
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         winston.format.printf(info => `${info.timestamp} - ${info.level.toUpperCase()}: ${info.message}`)
     ),
-    transports: [
-        new DailyRotateFile({
-            handleExceptions: true,
-            humanReadableUnhandledException: true,
-            dirname: logDir,
-            filename: 'angularcms.%DATE%.log', //Filename to be used to log to. This filename can include the %DATE% placeholder which will include the formatted datePattern at that point in the filename. (default: 'winston.log.%DATE%)
-            datePattern: "YYYY-MM-DD",
-            zippedArchive: true, //A boolean to define whether or not to gzip archived log files. (default 'false')
-            maxSize: "20m", //Maximum size of the file after which it will rotate. This can be a number of bytes, or units of kb, mb, and gb. If using the units, add 'k', 'm', or 'g' as the suffix. The units need to directly follow the number. (default: null)
-            maxFiles: `${CONFIG.LOG.KEEP_IN_DAYS}d` //Maximum number of logs to keep. If not set, no logs will be removed. This can be a number of files or number of days. If using days, add 'd' as the suffix. (default: null)
-        })
-    ],
+    transports: getTransports()
 };
 
 export const logger = winston.createLogger(options);
-
-// logger.stream = <any>{
-//     write: function (message, encoding) {
-//         const messageObj: MorganToken = JSON.parse(message);
-
-//         let code = messageObj.status;
-//         if (code < 400) {
-//             logger.info(`${code} - ${messageObj.url}`);
-//         } else {
-//             logger.error('', new HttpException(code, `${code} - ${messageObj.url}`));
-//         }
-//     }
-// };
 
 class LoggerStream {
     write(message: string) {
