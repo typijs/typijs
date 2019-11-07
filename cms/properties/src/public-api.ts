83ca913e51a22ac419fe8bb520d3a25caa3a4ec6
hung.dang <danghung1202@gmail.com>
2019-11-07 18:25:04 +0700

    Build Cms Properties project as Angular Library

---
+++ cms/properties/src/public-api.ts
@@ -0,0 +1,16 @@
+/*
+ * Public API Surface of properties
+ */
+export * from './input/input.component';
+
+export * from './content-area/content-area.component';
+export * from './textarea/textarea.component';
+export * from './property-list/property-list.component';
+export * from './select/dropdown/dropdown.component';
+export * from './select/checkbox/checkbox-group.control';
+export * from './xhtml/tinymce.component'
+
+export * from './content-reference/content-reference.component';
+export * from './content-reference/content-reference.control';
+
+export * from './properties.module';
