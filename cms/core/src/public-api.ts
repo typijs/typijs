fbd4a7171de7a18689c4b60cbc430f8e74fa51a6
hung.dang <danghung1202@gmail.com>
2019-11-07 18:23:10 +0700

    Build core as Angular Library

---
+++ cms/core/src/public-api.ts
@@ -0,0 +1,51 @@
+/*
+ * Public API Surface of core
+ */
+
+export * from './utils/service-locator';
+export * from './utils/outside-zone-event-plugin';
+export * from './utils/route-reuse-strategy';
+
+export * from './bases/cms-component';
+export * from './bases/cms-property';
+export * from './bases/content-data';
+export * from './bases/page-data'
+export * from './bases/select-item';
+export * from './bases/selection-factory';
+
+export * from './render/cms-content';
+
+export * from './constants/module-config';
+export * from './constants/ui-hint'
+export * from './constants/meta-keys';
+
+export * from './decorators/content-type-metadata';
+export * from './decorators/block-type.decorator';
+export * from './decorators/page-type.decorator';
+export * from './decorators/property.decorator';
+export * from './decorators/validate.decorator';
+
+export * from './directives/insert-point.directive';
+export * from './directives/content-area.directive';
+
+export * from './models/content.model';
+export * from './models/block.model';
+export * from './models/content.model';
+export * from './models/media.model';
+
+export { slugify, sortTabByTitle, uniqueId, clone } from './helpers/common';
+
+export * from './services/content.service';
+export * from './services/block.service';
+export * from './services/page.service';
+export * from './services/subject.service';
+export * from './services/media.service';
+
+export * from './shared/dnd/droppable.directive';
+export * from './shared/dnd/draggable.directive';
+export * from './shared/dnd/dnd-placeholder.directive';
+export * from './shared/dnd/dnd.module';
+
+export { CMS } from './cms';
+export * from './core.module';
+export * from './angular-cms.module';
