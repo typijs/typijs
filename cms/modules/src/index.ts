c88b332a80629e802c35438bba21d4c27a7cfac8
hung.dang <danghung1202@gmail.com>
2019-11-11 12:51:46 +0700

    Remove Barrel Import from Cms module, ng build don't like it

--- cms/modules/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './block';
-export * from './page';
-export * from './layout';
-export * from './media';
-export * from './site-manage';
-export * from './shared/angular-split';
--- cms/modules/src/shared/tree/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './tree-menu';
-export * from './tree-config';
-export * from './tree-node';
-export * from './tree-service';
-export * from './tree.component';
