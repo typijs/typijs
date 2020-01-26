8c817095787ca9850ba1793ac393b1f91bff41d6
danghung1202 <danghung1202@gmail.com>
2020-01-26 22:28:24 +0700

    Cms-api: Router refactor

--- cms-api/src/modules/block/block.route.ts
+++ cms-api/src/modules/block/block.route.ts
@@ -4,14 +4,6 @@ import { BlockCtrl } from './block.controller';
 const block: Router = Router();
 const controller = new BlockCtrl();
 
-block.get('/:id', controller.get);
-
-block.post('/', controller.insert);
-
-block.put('/:id', controller.update);
-
-block.delete('/:id', controller.delete);
-
 block.get('/folders/:parentId?', controller.getFoldersByParentId);
 
 block.get('/children/:parentId?', controller.getContentsByFolder);
@@ -19,5 +11,13 @@ block.get('/children/:parentId?', controller.getContentsByFolder);
 block.post('/folder', controller.createFolderContent);
 
 block.put('/folder/:id', controller.updateFolderName);
+
+block.get('/:id', controller.get);
+
+block.post('/', controller.insert);
+
+block.put('/:id', controller.update);
+
+block.delete('/:id', controller.delete);
 
 export { block };
--- cms-api/src/routes.ts
+++ cms-api/src/routes.ts
@@ -9,17 +9,6 @@ import { sideDefinition } from './modules/site-definition/site-definition.route'
 
 const appRouter: Router = Router();
 
-// Contents
-// const contentCtrl = new ContentCtrl();
-// appRouter.route('/contents').get(contentCtrl.getAll);
-// appRouter.route('/contents/count').get(contentCtrl.count);
-// appRouter.route('/content').post(contentCtrl.insert);
-// appRouter.route('/content/:id').get(contentCtrl.get);
-// appRouter.route('/content/:id').put(contentCtrl.update);
-// appRouter.route('/content/:id').delete(contentCtrl.delete);
-// appRouter.route('/content-by-url').get(contentCtrl.getByUrl);
-// appRouter.route('/contents-by-parent/:parentId').get(contentCtrl.getAllByParentId);
-
 appRouter.use('/page', page);
 
 // Blocks
