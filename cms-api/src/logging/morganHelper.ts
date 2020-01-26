abe122f1bfbe00fa01e1ce3d072717481d3cef68
danghung1202 <danghung1202@gmail.com>
2020-01-26 20:45:04 +0700

    Using nameOfFactory to get property name

--- cms-api/src/logging/morganHelper.ts
+++ cms-api/src/logging/morganHelper.ts
@@ -1,4 +1,4 @@
-import { nameof } from "../utils";
+import { nameof, nameofFactory } from "../utils";
 
 export enum MorganLogFormat {
     Combined = "combined",
@@ -22,20 +22,21 @@ export interface MorganToken {
     userAgent?: string
 }
 
+const morganToken = nameofFactory<MorganToken>();
 //Output '{"status"\:":status","remote-addr"\:":remote-addr","remote-user"\:":remote-user","method"\:":method","url"\:":url","http"\:":http-version","referrer"\:":referrer","agent"\:":user-agent","length"\:":res[content-length]"}'
 export function morganJsonFormat(tokens, req, res) {
     const jsonBody = [
-        `"${nameof<MorganToken>("method")}"\:"${tokens.method(req, res)}"`,
-        `"${nameof<MorganToken>("url")}"\:"${tokens.url(req, res)}"`,
-        `"${nameof<MorganToken>("status")}"\:"${tokens.status(req, res)}"`,
-        `"${nameof<MorganToken>("contentLength")}"\:"${tokens.res(req, res, 'content-length')}"`,
-        `"${nameof<MorganToken>("responseTime")}"\:"${tokens['response-time'](req, res)} ms"`,
-        `"${nameof<MorganToken>("date")}"\:"${tokens.date(req, res)}"`,
-        `"${nameof<MorganToken>("urlReferrer")}"\:"${tokens.referrer(req, res)}"`,
-        `"${nameof<MorganToken>("remoteAddress")}"\:"${tokens['remote-addr'](req, res)}"`,
-        `"${nameof<MorganToken>("remoteUser")}"\:"${tokens['remote-user'](req, res)}"`,
-        `"${nameof<MorganToken>("userAgent")}"\:"${tokens['user-agent'](req, res)}"`,
-        `"${nameof<MorganToken>("httpVersion")}"\:"${tokens['http-version'](req, res)}"`
+        `"${morganToken("method")}"\:"${tokens.method(req, res)}"`,
+        `"${morganToken("url")}"\:"${tokens.url(req, res)}"`,
+        `"${morganToken("status")}"\:"${tokens.status(req, res)}"`,
+        `"${morganToken("contentLength")}"\:"${tokens.res(req, res, 'content-length')}"`,
+        `"${morganToken("responseTime")}"\:"${tokens['response-time'](req, res)} ms"`,
+        `"${morganToken("date")}"\:"${tokens.date(req, res)}"`,
+        `"${morganToken("urlReferrer")}"\:"${tokens.referrer(req, res)}"`,
+        `"${morganToken("remoteAddress")}"\:"${tokens['remote-addr'](req, res)}"`,
+        `"${morganToken("remoteUser")}"\:"${tokens['remote-user'](req, res)}"`,
+        `"${morganToken("userAgent")}"\:"${tokens['user-agent'](req, res)}"`,
+        `"${morganToken("httpVersion")}"\:"${tokens['http-version'](req, res)}"`
     ].join(',');
 
     return `{${jsonBody}}`;
--- cms-api/src/utils/index.ts
+++ cms-api/src/utils/index.ts
@@ -1,3 +1,4 @@
-export function nameof<T>(key: keyof T, instance?: T): keyof T {
-    return key;
-}
+//Safe way to extract property names
+//https://schneidenbach.gitbooks.io/typescript-cookbook/nameof-operator.html
+export const nameof = <T>(name: keyof T) => name;
+export const nameofFactory = <T>() => (name: keyof T) => name;
