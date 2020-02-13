115686bc0d721e80fd5d918837315e793870da52
hung.dang <danghung1202@gmail.com>
2020-02-13 18:32:23 +0700

    cms-property: Content Area: fix bug

--- cms/modules/src/properties/content-area/content-area.control.ts
+++ cms/modules/src/properties/content-area/content-area.control.ts
@@ -1,5 +1,6 @@
 import { Component, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Content } from '@angular-cms/core';
 
 function generateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -8,19 +9,36 @@ function generateUUID() {
     });
 }
 
+export interface ContentAreaItem {
+    //mongoose Object id
+    _id?: string;
+    //guid id in this content area
+    guid?: string;
+    //belong to which content area
+    owner?: string;
+    name?: string;
+    contentType?: string;
+    //type of content area item
+    type?: string;
+}
+
 @Component({
-    selector: 'content-group',
+    selector: 'content-area',
     template: `
             <ul class="list-group" droppable (onDrop)="onDropItem($event)">
-                <li *ngFor="let item of model;" draggable [dragData]="item" class="list-group-item d-flex list-group-item-action justify-content-between align-items-center">
+                <li class="list-group-item d-flex list-group-item-action justify-content-between align-items-center" 
+                    *ngFor="let item of model;" 
+                    draggable 
+                    [dragData]="item">
                     <div>
                         <i class="fa fa-comment fa-fw"></i> {{item.name}}
-                        <span class="pull-right text-muted small"><em>{{item.value}}</em>
-                        </span>
                     </div>
                 </li>
-                <li dndPlaceholder>
-                    This is dnd placeholder
+                <li class="list-group-item d-flex list-group-item-action justify-content-between align-items-center"
+                    dndPlaceholder>
+                    <div>
+                        <i class="fa fa-comment fa-fw"></i> Drop here
+                    </div>
                 </li>
             </ul>
 `,
@@ -33,16 +51,17 @@ function generateUUID() {
     ]
 })
 export class ContentAreaControl implements ControlValueAccessor {
-    private _model: any;
+    private _model: ContentAreaItem[];
     private onChange: (m: any) => void;
     private onTouched: (m: any) => void;
 
     @Input() name: string;
 
-    get model() {
+    get model(): ContentAreaItem[] {
         return this._model;
     }
-
+    //Writes a new value to the element.
+    //This method is called by the forms API to write to the view when programmatic changes from model to view are requested.
     writeValue(value: any): void {
         this._model = value;
         if (this._model) {
@@ -63,20 +82,28 @@ export class ContentAreaControl implements ControlValueAccessor {
 
     onDropItem(e: any) {
         if (!this._model) this._model = [];
-        let item = e.dragData;
+        //TODO: emit unnecessary field when drop 
+        const itemIndex = e.index;
+        const { _id, name, contentType, isPublished }: Partial<Content> = e.dragData;
+        const item: Partial<ContentAreaItem> = {
+            _id: _id,
+            name: name,
+            contentType: contentType,
+        };
 
         if (item.owner == this.name) {
-            let oldGuid = item.guid;
+            const oldGuid = item.guid;
             item.guid = generateUUID();
-            this._model.splice(e.index, 0, item);
-            let existIndex = this._model.findIndex(x => x.guid == oldGuid);
+
+            this._model.splice(itemIndex, 0, item);
+            const existIndex = this._model.findIndex(x => x.guid == oldGuid);
             if (existIndex != -1) {
                 this._model.splice(existIndex, 1);
             }
         } else {
             item.guid = generateUUID();
             item.owner = this.name;
-            this._model.splice(e.index, 0, item);
+            this._model.splice(itemIndex, 0, item);
         }
 
         this.onChange(this._model);
