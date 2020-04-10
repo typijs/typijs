eb2849ff9e3c2ff710f890b1e541821645c96bc1
charles443 <dang_viet_hung_1988@yahoo.com>
2020-04-10 23:22:02 +0700

    [xhtml] drop to insert page url, image into quill editor

--- cms/modules/src/properties/xhtml/xhtml.property.ts
+++ cms/modules/src/properties/xhtml/xhtml.property.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
+import Quill from 'quill'
 import { QuillEditorComponent } from 'ngx-quill';
 
-import { CmsProperty } from '@angular-cms/core';
+import { CmsProperty, PAGE_TYPE, MEDIA_TYPE } from '@angular-cms/core';
 import { DropEvent } from '../../shared/drag-drop/drop-event.model';
 
 @Component({
@@ -25,14 +26,50 @@ export class XHtmlProperty extends CmsProperty {
         static: true
     }) editor: QuillEditorComponent
 
-    onDropItem(e: DropEvent) {
-        const { _id, id, name, owner, guid, extendProperties, type, contentType, isPublished } = e.dragData;
+    constructor() {
+        super();
+        //TODO how using inline style in quill editor
+        console.log(Quill.imports);
+    }
 
-        const src = `http://localhost:3000/api/assets/${_id}/${name}`;
-        if (this.editor.quillEditor) {
-            const quillEditor = this.editor.quillEditor;
-            const range = quillEditor.getSelection(true);
-            quillEditor.insertEmbed(range.index, 'image', src, 'user')
+    onDropItem(e: DropEvent) {
+        const { type, extendProperties } = e.dragData;
+        const typeOfContent = extendProperties ? extendProperties.type : type;
+        switch (typeOfContent) {
+            case PAGE_TYPE:
+                this.insertPageUrl(e.dragData);
+                break;
+            case MEDIA_TYPE:
+                this.insertImageUrl(e.dragData);
+                break;
         }
+    }
+
+    private insertImageUrl(dragData) {
+        const { _id, name } = dragData;
+        const src = `http://localhost:3000/api/assets/${_id}/${name}`;
+        const insertOps = [
+            { insert: { image: src }, attributes: { width: '150' } }
+        ]
+        this.quillInsertAtCursorPosition(this.editor.quillEditor, insertOps);
+    }
+
+    private insertPageUrl(dragData) {
+        const { name, extendProperties } = dragData;
+        const insertOps = [
+            { insert: name, attributes: { link: extendProperties.linkUrl } }
+        ]
+        this.quillInsertAtCursorPosition(this.editor.quillEditor, insertOps);
+    }
+
+    private quillInsertAtCursorPosition(quillEditor: any, insertOperators: { insert: any, attributes: any }[]) {
+        if (!quillEditor) return;
+
+        const operators: any[] = [];
+        const range = quillEditor.getSelection(true);
+        if (range.index > 0) operators.push({ retain: range.index });
+
+        insertOperators.forEach(insert => operators.push(insert));
+        quillEditor.updateContents({ ops: operators });
     }
 }
