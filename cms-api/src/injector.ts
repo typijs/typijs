60e7b8d633b13750876957c85760398e8a7f0aeb
angularxcms <angularxcms@gmail.com>
2020-06-05 15:03:44 +0700

    Injector provider rename

--- cms-api/src/injector.ts
+++ cms-api/src/injector.ts
@@ -1,7 +1,7 @@
 import { ReflectiveInjector } from "injection-js";
-import { ResolvePageProviders } from './modules/page/page-providers';
-import { ResolveBlockProviders } from './modules/block/block-providers';
-import { ResolveMediaProviders } from './modules/media/media-providers';
+import { ResolvePageProviders } from './modules/page/page.providers';
+import { ResolveBlockProviders } from './modules/block/block.providers';
+import { ResolveMediaProviders } from './modules/media/media.providers';
 
 //TODO: should has the setting providers in app express to allow override
 export const injector = ReflectiveInjector.fromResolvedProviders([
