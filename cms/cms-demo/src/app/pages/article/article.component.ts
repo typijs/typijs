efdd6c100d2b852b1f967cb2341c2f2b9f80a813
angularxcms <angularxcms@gmail.com>
2020-06-23 11:12:10 +0700

    [cms-demo] clean up

--- cms/cms-demo/src/app/pages/article/article.component.ts
+++ cms/cms-demo/src/app/pages/article/article.component.ts
@@ -5,7 +5,6 @@ import { ArticlePage } from './article.pagetype';
 @Component({
     templateUrl: 'article.component.html'
 })
-
 export class ArticleComponent extends CmsComponent<ArticlePage> {
     constructor() {
         super();
