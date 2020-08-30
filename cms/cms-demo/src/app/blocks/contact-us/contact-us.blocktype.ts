2dc5f366ce1287c10562fcc9822c40067609db5e
danghung1202 <danghung1202@gmail.com>
2020-08-30 13:57:55 +0700

    [cms] Refactor

--- cms/cms-demo/src/app/blocks/contact-us/contact-us.blocktype.ts
+++ cms/cms-demo/src/app/blocks/contact-us/contact-us.blocktype.ts
@@ -1,12 +1,29 @@
-import { BlockType } from '@angular-cms/core';
+import { BlockType, Property, UIHint } from '@angular-cms/core';
 import { ContactUsComponent } from './contact-us.component';
 import { LinkListBlock } from '../link-list/link-list.blocktype';
 
 @BlockType({
-    displayName: "Contact Us Block",
+    displayName: 'Contact Us Block',
     componentRef: ContactUsComponent,
-    description: "Contact Us Block"
+    description: 'Contact Us Block'
 })
 export class ContactUsBlock extends LinkListBlock {
+    @Property({
+        displayName: 'Test Field',
+        displayType: UIHint.Text
+    })
+    testField: string;
+}
 
+@BlockType({
+    displayName: 'Contact Block 2',
+    componentRef: ContactUsComponent,
+    description: 'Contact Block 2'
+})
+export class ContactUsBlock2 extends LinkListBlock {
+    @Property({
+        displayName: 'Other Field',
+        displayType: UIHint.Text
+    })
+    otherField: string;
 }
