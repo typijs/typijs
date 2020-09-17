4cb2e32e98b3e8ccf604329a399637307e8ac499
danghung1202 <danghung1202@gmail.com>
2020-09-17 14:25:22 +0700

    [cms-modules] Refactor

--- cms/core/src/decorators/property.decorator.ts
+++ cms/core/src/decorators/property.decorator.ts
@@ -4,6 +4,7 @@ import { Validators } from '@angular/forms';
 import { ClassOf } from '../types';
 import { PROPERTIES_METADATA_KEY, PROPERTY_METADATA_KEY } from './metadata-key';
 import { ISelectionFactory } from '../bases/selection-factory';
+import { UIHint } from '../types/ui-hint';
 
 export type ValidateMetadata = {
     validateFn: Function;
@@ -33,17 +34,43 @@ export interface PropertyMetadata {
  * https://www.typescriptlang.org/docs/handbook/decorators.html#decorator-factories
  * @param metadata
  */
-export function Property(metadata: PropertyMetadata): PropertyDecorator {
+export function Property(metadata: PropertyMetadata = {}): PropertyDecorator {
     function propertyDecorator(target: object, propertyKey: string) {
         const properties: string[] = Reflect.getOwnMetadata(PROPERTIES_METADATA_KEY, target.constructor) || [];
         if (properties.indexOf(propertyKey) === -1) { properties.push(propertyKey); }
         Reflect.defineMetadata(PROPERTIES_METADATA_KEY, properties, target.constructor);
 
-        // Obtaining type metadata using the reflect metadata API
-        const propertyTypeMetadata = Reflect.getMetadata('design:type', target, propertyKey);
-        if (propertyTypeMetadata) { Object.assign(metadata, { _propertyType: propertyTypeMetadata.name }); }
+        if (!metadata._propertyType) {
+            metadata._propertyType = getPropertyType(target, propertyKey);
+        }
+        if (!metadata.displayType) {
+            metadata.displayType = getDefaultDisplayType(metadata._propertyType);
+        }
+        if (!metadata.displayName) {
+            metadata.displayName = propertyKey;
+        }
 
         return Reflect.defineMetadata(PROPERTY_METADATA_KEY, metadata, target.constructor, propertyKey);
+    }
+
+    function getPropertyType(target: object, propertyKey: string): string {
+        // Obtaining type metadata using the reflect metadata API
+        const propertyTypeMetadata = Reflect.getMetadata('design:type', target, propertyKey);
+        return propertyTypeMetadata ? propertyTypeMetadata.name : undefined;
+    }
+
+    function getDefaultDisplayType(propertyType: string): string {
+        if (!propertyType) { return undefined; }
+        switch (propertyType.toLowerCase()) {
+            case 'string':
+                return UIHint.Text;
+            case 'boolean':
+                return UIHint.Checkbox;
+            case 'number':
+                return UIHint.Text;
+            default:
+                return undefined;
+        }
     }
 
     return propertyDecorator;
