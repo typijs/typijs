a2d54fff9c7a5d7fb151c35e5648faa7aed4ac95
danghung1202 <danghung1202@gmail.com>
2020-12-01 14:37:33 +0700

    [cms-core] Add the PAGE AFTER INIT token to allow the function execute after the page component view init

--- cms/cms-demo/src/app/app.module.ts
+++ cms/cms-demo/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Renderer2, RendererFactory2 } from '@angular/core';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 
-import { AngularCms, AuthModule } from '@angular-cms/core';
+import { AngularCms, AuthModule, PAGE_AFTER_INIT } from '@angular-cms/core';
 
 import * as contentTypes from './register-content-types';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,21 +14,40 @@ import { HttpClientModule } from '@angular/common/http';
 
 AngularCms.registerContentTypes(contentTypes);
 
+// Use a factory that return an array of dependant functions to be executed
+export function pageAfterViewInit(rendererFactory: RendererFactory2) {
+    return () => {
+        const renderer = rendererFactory.createRenderer(null, null);
+        const existedScript = document.getElementById('secondary-js');
+        if (existedScript) {
+            renderer.removeChild(document.body, existedScript);
+        }
+        const script = renderer.createElement('script');
+        script.id = 'secondary-js';
+        script.type = 'text/javascript';
+        script.src = 'assets/js/secondary.js';
+        renderer.appendChild(document.body, script);
+    };
+}
+
 @NgModule({
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    TransferHttpCacheModule,
-    HttpClientModule,
-    AngularCms.forRoot(),
-    AuthModule,
-    AppRoutingModule,
-    PagesModule,
-    BlocksModule
-  ],
-  declarations: [
-    AppComponent,
-    LayoutComponent,
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        TransferHttpCacheModule,
+        HttpClientModule,
+        AngularCms.forRoot(),
+        AuthModule,
+        AppRoutingModule,
+        PagesModule,
+        BlocksModule
+    ],
+    declarations: [
+        AppComponent,
+        LayoutComponent,
+    ],
+    providers: [
+        { provide: PAGE_AFTER_INIT, useFactory: pageAfterViewInit, deps: [RendererFactory2], multi: true }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- cms/core/src/cms.initializer.ts
+++ cms/core/src/cms.initializer.ts
@@ -5,7 +5,7 @@ import { ConfigService } from './config/config.service';
 import { AuthService } from './auth/auth.service';
 import { authCheckFactory } from './auth/auth.factory';
 
-export const CONFIG_DEPS = new InjectionToken<(() => Function)[]>('CONFIG_DEPENDENCIES');
+export const CONFIG_DEPS = new InjectionToken<(() => void)[]>('CONFIG_DEPENDENCIES');
 
 // Use a factory that return an array of dependant functions to be executed
 export function configDepsFactory(authService: AuthService, configService: ConfigService) {
@@ -13,7 +13,7 @@ export function configDepsFactory(authService: AuthService, configService: Confi
 }
 
 // https://medium.com/@gmurop/managing-dependencies-among-app-initializers-in-angular-652be4afce6f
-export function cmsInitializer(configService: ConfigService, configDeps: (() => Function)[]): () => Promise<any> {
+export function cmsInitializer(configService: ConfigService, configDeps: (() => void)[]): () => Promise<any> {
     return (): Promise<any> => new Promise((resolve, reject) => {
         configLoadFactory(configService)()
             .then(data => {
