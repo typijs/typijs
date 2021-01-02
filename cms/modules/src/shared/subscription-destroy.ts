77854267b6fb0ceae37fa9aaabefbca95f711fc2
danghung1202 <danghung1202@gmail.com>
2021-01-02 12:09:26 +0700

    update to angular v10

--- cms/modules/src/shared/subscription-destroy.ts
+++ cms/modules/src/shared/subscription-destroy.ts
@@ -1,6 +1,7 @@
 import { OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 
+// TODO: Add Angular decorator.
 export abstract class SubscriptionDestroy implements OnDestroy {
     protected unsubscribe$: Subject<any> = new Subject();
 
