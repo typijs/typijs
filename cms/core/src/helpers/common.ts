b1606547dea68f8ea93aaa1ae41debb91e06cccf
danghung1202 <danghung1202@gmail.com>
2021-02-09 22:01:03 +0700

    [cms]  add group by array func

--- cms/core/src/helpers/common.ts
+++ cms/core/src/helpers/common.ts
@@ -156,3 +156,40 @@ export function convertObjectToUrlQueryString(query: { [param: string]: string |
 export function isNullOrWhiteSpace(str: string): boolean {
     return str === null || str.match(/^ *$/) !== null;
 }
+
+
+/**
+ * Array group by method
+ *
+ * Usage:
+ * ```javascript
+ * const people = [
+    { name: 'Lee', age: 21 },
+    { name: 'Ajay', age: 20 },
+    { name: 'Jane', age: 20 }
+];
+ * const groupedPeople = groupBy(people, 'age');
+ * console.log(groupedPeople);
+ * ```
+ * Output:
+ * ```
+ * {
+    20: [{ name: 'Ajay', age: 20 }, { name: 'Jane', age: 20 }],
+    21: [ { name: 'Lee', age: 21 } ]
+ }
+ * ```
+ *
+ * @param objectArray
+ * @param property
+ */
+export function groupBy<T>(objectArray: T[], property: string): { [key: string]: T[] } {
+    return objectArray.reduce((acc, obj) => {
+        const key = obj[property];
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        // Add object to list for given key's value
+        acc[key].push(obj);
+        return acc;
+    }, {});
+}
